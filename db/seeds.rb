c3cbbdb395a4f171c523f80dbec4b42028e60c76
Renee Mumbi <1723renee@gmail.com>
2023-03-20 17:31:20 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+restaurant1 = Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
+restaurant2 = Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
+
+pizza1 = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+pizza2 = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+pizza3 = Pizza.create(name: "Hawaiian", ingredients: "Dough, Tomato Sauce, Cheese, Ham, Pineapple")
+
+
+restaurant1.restaurant_pizzas.create(pizza: pizza1, price: 16.99)
+restaurant2.restaurant_pizzas.create(pizza: pizza2, price: 18.99)
+
