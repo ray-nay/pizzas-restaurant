9def01e5ea03aff20255b4c0bbc80152cab9fd77
Renee Mumbi <1723renee@gmail.com>
2023-03-20 14:47:03 +0300

    Add validation to Restpizza model

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,6 @@
 class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
+
+  validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 30 }
 end
