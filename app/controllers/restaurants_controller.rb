f0826262ee2f8bb6cee28c3885b2dd188528dbc6
Renee Mumbi <1723renee@gmail.com>
2023-03-20 16:46:34 +0300

    Add showre and destroy methods for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,20 @@ class RestaurantsController < ApplicationController
         restaurants = Restaurant.all 
         render json: restaurants
     end 
+    def show 
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant
+    end 
+
+    def destroy
+        restaurant = Restaurant.find(params[:id])
+        restaurant.destroy
+        head :no_content
+    end
+
+
+    private 
+    def render_not_found_response
+        render json: {error:"Restaurant not found"}, status: :not_found
+     end 
 end
