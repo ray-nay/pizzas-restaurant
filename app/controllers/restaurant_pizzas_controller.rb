f9f487963c5d2db2311da9d0e49e95943925c1ba
Renee Mumbi <1723renee@gmail.com>
2023-03-20 17:32:31 +0300

    modify restaurant pizza controller

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantPizzasController < ApplicationController
+    def create
+        rest_pizza = RestaurantPizza.create!(restaurant_pizzas_params)
+
+        if rest_pizza.valid?
+        render json: rest_pizza, status: :created, except: [:created_at, :updated_at]
+        else
+            render json: { "errors": ["validation errors"] }, status: :unprocessable_entity
+    end 
+
+    private 
+    def restaurant_pizzas_params
+        params.permit(:price, :pizza_id, :restaurant_id)
+    end 
 end
